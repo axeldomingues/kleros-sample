ee0234da5c86251cd2ef91bd18cd3c15c9f7fb2e
Axel Domingues <adomingues@guidewire.com>
2020-09-25 14:33:15 +0100

    Kleros sample subgraph implementation

---
+++ README.md
@@ -0,0 +1,15 @@
+# The Graph - Kleros Sample
+
+This is a sample implementation of a simple subgraph of The Graph protocol. This sample demonstrates how easy is to index and query data from blockchains which, otherwise would be difficult to query directly.
+
+In this specific example, we are indexing Kleros, which is a blockchain dispute resolution layer. The goal is to index and query disputes and phases. In this exercise, we want to ask a straightforward question that would be difficult to answer without The Graph protocol. For example, how many disputes exist per phase and which specific disputes exist per phase.
+
+## Contracts
+
+Below the graph dashboard of this sample implementation
+* [Subgraph dashboard](https://thegraph.com/explorer/subgraph/axeldomingues/kleros-sample?query=Disputes%20array%20per%20phase)
+ 
+## Contracts
+
+Eth mainnet indexed contract
+* [Kleros Liquid](https://etherscan.io/address/0x988b3a538b618c7a603e1c11ab82cd16dbe28069#code)
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, log } from "@graphprotocol/graph-ts"
 import {
   Contract,
   NewPhase,
@@ -10,83 +10,44 @@ import {
   AppealPossible,
   AppealDecision
 } from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import { Dispute, Period } from "../generated/schema"
 
-export function handleNewPhase(event: NewPhase): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleNewPhase(event: NewPhase): void {}
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+export function handleNewPeriod(event: NewPeriod): void {
+  let periodId = String.fromCharCode(event.params._period)
+  let disputeId = event.params._disputeID.toString()
+  let newPeriodEntity = Period.load(periodId)
+  let disputeEntity = Dispute.load(disputeId)
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  // check if this is a new period
+  if (newPeriodEntity != null) {
+    newPeriodEntity.period = event.params._period
+    newPeriodEntity.totalDisputes = newPeriodEntity.totalDisputes.plus(BigInt.fromI32(1))
+    log.info('Incremented dispute count', [periodId])
+  } else {
+    newPeriodEntity = new Period(periodId)
+    newPeriodEntity.period = event.params._period
+    newPeriodEntity.totalDisputes = BigInt.fromI32(1)
+    log.info('Initialized Period', [periodId])
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  // check if this new period is for an existing dispute
+  if (disputeEntity != null) {
+    // this new period is for an existing dispute; thus, we have to adjust the total disputes of this previous period
+    let previousPeriodEntity = Period.load(disputeEntity.period)
+    previousPeriodEntity.totalDisputes = previousPeriodEntity.totalDisputes.minus(BigInt.fromI32(1))
+    previousPeriodEntity.save()
+    log.info('Decremented dispute count', [periodId])
+  } else {
+    disputeEntity = new Dispute(disputeId)
+  }
 
-  // Entity fields can be set based on event parameters
-  entity._phase = event.params._phase
+  disputeEntity.period = periodId
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.RNBlock(...)
-  // - contract.disputesWithoutJurors(...)
-  // - contract.governor(...)
-  // - contract.lastDelayedSetStake(...)
-  // - contract.disputeStatus(...)
-  // - contract.maxDrawingTime(...)
-  // - contract.currentRuling(...)
-  // - contract.courts(...)
-  // - contract.ALPHA_DIVISOR(...)
-  // - contract.getSubcourt(...)
-  // - contract.onTransfer(...)
-  // - contract.disputes(...)
-  // - contract.RN(...)
-  // - contract.RNGenerator(...)
-  // - contract.NON_PAYABLE_AMOUNT(...)
-  // - contract.getVote(...)
-  // - contract.stakeOf(...)
-  // - contract.appealPeriod(...)
-  // - contract.phase(...)
-  // - contract.MAX_STAKE_PATHS(...)
-  // - contract.delayedSetStakes(...)
-  // - contract.lastPhaseChange(...)
-  // - contract.minStakingTime(...)
-  // - contract.nextDelayedSetStake(...)
-  // - contract.getJuror(...)
-  // - contract.onApprove(...)
-  // - contract.jurors(...)
-  // - contract.getDispute(...)
-  // - contract.getVoteCounter(...)
-  // - contract.MIN_JURORS(...)
-  // - contract.appealCost(...)
-  // - contract.lockInsolventTransfers(...)
-  // - contract.arbitrationCost(...)
-  // - contract.pinakion(...)
+  newPeriodEntity.save()
+  disputeEntity.save()
 }
-
-export function handleNewPeriod(event: NewPeriod): void {}
 
 export function handleStakeSet(event: StakeSet): void {}
 
